9a3ed8faaea7e05e8adab391f2083a0baaeb65ef
u6527282409-crypto <u6527282409@gmail.com>
2025-08-15 14:55:50 +0200

    Add files via upload

---
+++ 7c93fa6a/chunk.all-wallets-CfxR50CG.js
@@ -0,0 +1,6 @@
+import{a0 as a}from"./index.js";import"https://esm.sh/react@18.3.1";import"https://esm.sh/react-dom@18.3.1";import"https://esm.sh/viem@2.26.2";const r=a`<svg fill="none" viewBox="0 0 24 24">
+  <path
+    style="fill: var(--wui-color-accent-100);"
+    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
+  />
+</svg>`;export{r as allWalletsSvg};
